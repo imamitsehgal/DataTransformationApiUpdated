58c9f2e34d85b34df3e01e5da63b50018ba65cbb
imamitsehgal <imamitsehgal@gmail.com>
2017-02-07 09:37:36 +0530

    First commit with files

---
+++ src/main/resources/CommonFunctions.js
@@ -0,0 +1,4 @@
+function doubleMe(a) { return a + a ;}
+function concatUs(a,b) { return a.concat(b);}
+function getValidData(a) { if(a == 'NaN') {return '0'} else {return a };}
+function convertVSTradeIdToHTI(tradeType,tradeId) {var trauxId='000000237974'; return trauxId + tradeId ;}
